7aa54f8b2b19d119c4c103391023320d1314d7bb
josephkim93 <frogkim93@gmail.com>
2025-04-01 00:11:31 +0900

    테스트파일 추가
    메시지 확인을 위한 test, test2, test3 추가

--- src/main/java/com/recipeworld/recipeworld/repository/RecipeRepository.java
+++ src/main/java/com/recipeworld/recipeworld/repository/RecipeRepository.java
@@ -3,8 +3,5 @@ package com.recipeworld.recipeworld.repository;
 import com.recipeworld.recipeworld.entity.Recipe;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 public interface RecipeRepository extends MongoRepository<Recipe, String> {
-    Optional<Recipe> findByName(String name);
 }
